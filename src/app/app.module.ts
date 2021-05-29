fcd4d42107a40e4391ead8a881841aebe0e9acff
Nikita Zverev <nikitaz@backbase.com>
2021-05-29 21:58:21 +0200

    Added the initial structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {SignUpModule} from './sign-up/sign-up.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SignUpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
