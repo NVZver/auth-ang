b74ddf4ef4fe539c7d8e85c71eeaea6325b74d70
Nikita Zverev <nikitaz@backbase.com>
2021-05-31 13:56:55 +0200

    Added form validators

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -3,7 +3,7 @@ import {SingUpService} from './sing-up.service';
 import {SingUpRequestData, SingUpStatus} from './types';
 import {BehaviorSubject, Subject} from 'rxjs';
 import {takeUntil} from 'rxjs/operators';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -13,12 +13,15 @@ import {FormBuilder, FormGroup} from '@angular/forms';
 })
 export class SignUpComponent implements OnInit, OnDestroy {
 
-  singUpStatus$: BehaviorSubject<SingUpStatus|undefined> = new BehaviorSubject<SingUpStatus|undefined>(undefined);
+  singUpStatus$: BehaviorSubject<SingUpStatus | undefined> = new BehaviorSubject<SingUpStatus | undefined>(undefined);
 
   singUpForm: FormGroup = this.formBuilder.group({
-    firstName: [''],
-    lastName: [''],
-    email: ['']
+    firstName: ['', [Validators.required]
+    ],
+    lastName: ['', [Validators.required]
+    ],
+    email: ['', [Validators.required, Validators.email]
+    ]
   });
 
   readonly singUpStatus = SingUpStatus;
@@ -28,7 +31,8 @@ export class SignUpComponent implements OnInit, OnDestroy {
   constructor(
     private singUpService: SingUpService,
     private formBuilder: FormBuilder
-  ) { }
+  ) {
+  }
 
   ngOnInit(): void {
   }
@@ -39,16 +43,16 @@ export class SignUpComponent implements OnInit, OnDestroy {
   }
 
   singUp(): void {
+    this.singUpStatus$.next(this.singUpStatus.PROCESSING);
     this.singUpService.singUp(this.singUpForm.value).pipe(
       takeUntil(this.gc$)
     ).subscribe(res => {
       console.log(res);
-      if (res._id){
+      if (res._id) {
         this.singUpStatus$.next(SingUpStatus.SUCCESS);
       } else {
         this.singUpStatus$.next(SingUpStatus.ERROR);
       }
     });
   }
-
 }
